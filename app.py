406f385c3e28f062dc4bf17f38337732aab3ef6b
AnnaCarazza <acarazza@terpmail.umd.edu>
2024-02-19 21:21:30 -0500

    Deleted a mistake in app.py route because there were 2 "/hello" routes

--- app.py
+++ app.py
@@ -17,7 +17,3 @@ def about():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
-
-@app.route('/hello')
-def hello():  # put application's code here
-    return render_template('hello.html')
